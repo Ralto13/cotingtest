c8febe1dbf02747db17790d644a1488d0a3d7b7a
Ralto13 <doorumi13@gmail.com>
2024-10-11 01:02:09 +0900

    올바른 괄호

---
+++ programmers/Lv.2/올바른 괄호.js
@@ -0,0 +1,16 @@
+function solution(s) {
+
+  var correct = 0
+
+  for (let i = 0; i < s.length; i++) {
+
+    correct += s[i] === '(' ? 1 : -1
+
+    if (correct < 0) {
+      return false
+    }
+
+  }
+
+  return correct === 0;
+}
