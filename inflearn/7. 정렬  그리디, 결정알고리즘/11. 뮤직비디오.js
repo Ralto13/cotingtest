3ff8325f4905f26bd9097f7cf6613b48feb232f3
Ralto13 <doorumi13@gmail.com>
2025-01-21 01:32:53 +0900

    updates

--- inflearn/7. 정렬  그리디, 결정알고리즘/11. 뮤직비디오.js
+++ inflearn/7. 정렬  그리디, 결정알고리즘/11. 뮤직비디오.js
@@ -1,7 +1,33 @@
-function solution(m, songs){
-  let answer;
+// 
+// 
+
+function count(songs, capacity) {
+  let cnt = 1, sum = 0;
+  for (let x of songs) {
+    if (sum + x > capacity) {
+      cnt++;
+      sum = x;
+    } else sum += x;
+  }
+  return cnt;
+}
+
+function solution(m, songs) {
+  let answer = 0;
+  let lt = Math.max(...songs);
+  let rt = songs.reduce((acc, curr) => acc + curr);
+  
+  while (lt <= rt) {
+    let mid = parseInt((rt + lt) / 2);
+    if (count(songs, mid) <= m) {
+      answer = mid;
+      rt = mid - 1;
+    } else lt = mid + 1;
+
+  }
+  
   return answer;
 }
 
-let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(solution(3, arr));
